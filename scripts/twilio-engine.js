b3953ab85bb63cc5537063be09a6c0dff7c90ad7
Phoebe <phoebe.gao@mycase.com>
2017-07-27 21:29:58 -0700

    send texts! :)

---
+++ scripts/twilio-engine.js
@@ -0,0 +1,31 @@
+// Description:
+//   Allows Hubot to send text messages using Twilio API
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   TWILIO_ACCOUNT_SID
+//   TWILIO_ACCOUNT_TOKEN
+//   TWILIO_PHONE_NUMBER
+
+var twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_ACCOUNT_TOKEN);
+
+module.exports = {
+  sms: (to, body) => {
+    twilio.messages.create({ 
+      to, 
+      from: process.env.TWILIO_PHONE_NUMBER, 
+      body, 
+    }, function(err, message) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      if (message) {
+        console.log(message.sid); 
+      }
+    });
+  },
+};
